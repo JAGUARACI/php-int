4c82d570613ad3bd5c9390db343f48734d1efacf
JAGUARACI <jaguaracisudu@gmaile.com>
2019-10-11 21:10:00 -0300

    introdução HP

--- index.php
+++ index.php
@@ -5,7 +5,22 @@ $categorias[] = 'adolescente';
 $categorias[] = 'adultos';
 $categorias[] = 'idoso';
 
-print_r($categorias);
+//print_r($categorias);
+
+$nome = 'Eduardo';
+$idade = [];
+
+//var_dump ($nome);
+//var_dump ($idade);
+
+If($idade >= 6 && $idade <=12)
+
+{echo 'infantil';}
+
+else if($idade >= 13 && $idade <=18)
+
+{echo 'adolescente';}
+
 
 
 ?>
