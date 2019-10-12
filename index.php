54bce961e9188c9bc76006ef2f079809ba710333
JAGUARACI <jaguaracisudu@gmaile.com>
2019-10-11 22:19:58 -0300

    correção na linha 21

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ If($idade >= 6 && $idade <=12)
 {for($i = 0; $i <= count ($categorias); $i++)
 
 {if($categorias[$i] == 'infantil')
-echo "O nadador",$nome, "compete na categoria",$categorias[$1];
+echo "O nadador",$nome, "compete na categoria",$categorias[$i];
 }}
 
 
